5f09bdbdaa04ce25678c35446623775aa07b3a1e
timur0108 <timur.sirazitdinov@gmail.com>
2024-03-27 18:55:22 +0200

    2

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,19 +1,31 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
 
 public class UI extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+
+        Group root = new Group();
+
+        Scene scene  = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("File converter");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
