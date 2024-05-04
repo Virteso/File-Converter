88d17373bdf3f71afbf8b30990b2de78ca4de2ea
timur0108 <timur.sirazitdinov@gmail.com>
2024-05-04 19:08:12 +0300

    UI and CSV to PDF

--- src/main/java/org/converter/Converter.java
+++ src/main/java/org/converter/Converter.java
@@ -7,21 +7,157 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.apache.pdfbox.rendering.PDFRenderer;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
-
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class Converter {
+
+    public static void SCVtoPDF(String inputpath, String outputpath) {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(inputpath));
+
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            File output = new File(outputpath);
+            if (!output.exists()) output.mkdirs();
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            PDType1Font font = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
+            int fontsize = 12;
+
+            float y = page.getMediaBox().getHeight() - 50;
+            double biggestCell = 0;
+            String row;
+            String pikimRida = "";
+            double maxWidth = 0;
+            Map<Integer, Float> columnAndSize = new HashMap<>();
+            while ((row = csvReader.readLine()) != null) {
+                String[] rida = row.split(",");
+                for (int i = 0; i < rida.length; i++) {
+                    if (columnAndSize.containsKey(i)) {
+                        if (font.getStringWidth(rida[i]) > columnAndSize.get(i)) {
+                            columnAndSize.put(i, font.getStringWidth(rida[i]));
+                        }
+                    } else {
+                        columnAndSize.put(i, font.getStringWidth(rida[i]));
+                    }
+                }
+                double lineWidth = 0;
+                for (String s : rida) {
+                    double cellSize = font.getStringWidth(s);
+                    if (cellSize > biggestCell) biggestCell = cellSize;
+                    lineWidth += cellSize;
+                }
+                if (lineWidth > maxWidth) {
+                    maxWidth = lineWidth;
+                    pikimRida = row;
+                }
+
+            }
+            double pageWidth = page.getMediaBox().getWidth() - 100;
+            while(maxWidth >= pageWidth && fontsize > 1) {
+                fontsize --;
+                maxWidth = font.getStringWidth(pikimRida) / 1000 * fontsize;
+            }
+            contentStream.setFont(font, fontsize);
+            csvReader.close();
+            csvReader = new BufferedReader(new FileReader(inputpath));
+
+
+            while ((row = csvReader.readLine()) != null) {
+                String[] cells = row.split(",");
+                float x = 20;
+                int column = 0;
+
+                if (y - (fontsize + 10) < 20) {
+                    contentStream.close();
+                    page = new PDPage();
+                    document.addPage(page);
+                    contentStream = new PDPageContentStream(document, page);
+                    contentStream.setFont(font, fontsize);
+                    y = page.getMediaBox().getHeight() - 50;
+                }
+
+                for (String cell : cells) {
+                    contentStream.beginText();
+                    contentStream.newLineAtOffset(x, y);
+                    contentStream.showText(cell);
+                    contentStream.endText();
+                    x += columnAndSize.get(column)/1000 * fontsize + 10;
+                    //x += biggestCell / 1000 * fontsize + 10;
+                    //contentStream.moveTo(x, page.getMediaBox().getHeight() - 30);
+                    //contentStream.lineTo(x - 5, y - font.getHeight(0) * cells.length - 5);
+                    //contentStream.stroke();
+                    column ++;
+                }
+                //contentStream.lineTo(x - 5, y - font.getHeight(0) * cells.length - 5);
+                //contentStream.stroke();
+                //contentStream.moveTo(45, y - font.getHeight(0) * cells.length - 5); // Move to the starting point of the line
+                //contentStream.lineTo(x - 65, y - font.getHeight(0) * cells.length - 5); // Draw the line
+                //contentStream.stroke();
+                y -= 20;
+
+            }
+
+
+            contentStream.close();
+            document.save(outputpath + File.separator + "output.pdf");
+            document.close();
+            csvReader.close();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void extractPDFspreadsheets (String inputpath, String outputpath) {
+        try (PDDocument document = Loader.loadPDF(new File(inputpath))){
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            File output = new File(outputpath);
+            if (!output.exists()) output.mkdirs();
+            String text = pdfStripper.getText(document);
+            String[] lines = text.split("\\r?\\n");
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputpath + File.separator + "output.csv"))) {
+                for (String line : lines) {
+                    String[] columns = line.split(" ");
+                    for (int i = 0; i < columns.length; i++) {
+                        writer.write(columns[i]);
+                        if (i < columns.length - 1) {
+                            writer.write(",");
+                        }
+                    }
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     
     public static void PDFToPNG(String inputPath, String outputPath) {
         try {
             PDDocument document = Loader.loadPDF(new File(inputPath));
             PDFRenderer pdfRenderer = new PDFRenderer(document);
+
+            File output = new File(outputPath);
+            if (!output.exists()) {
+                output.mkdirs();
+            }
 
             for (int page = 0; page < document.getNumberOfPages(); ++page) {
                 BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300);
@@ -37,8 +173,7 @@ public class Converter {
     public static void PNGTextToPDFConverter(String inputPath, String outputPath, String language) {
         try {
             ITesseract tesseract = new Tesseract();
-
-            tesseract.setDatapath("src/main/resources/tessdata");
+            tesseract.setDatapath("File-Converter/src/main/resources/tessdata");
             tesseract.setLanguage(language);
 
             String text = tesseract.doOCR(new File(inputPath));
@@ -46,6 +181,9 @@ public class Converter {
             PDDocument document = new PDDocument();
             PDPage page = new PDPage();
             document.addPage(page);
+
+            File output = new File(outputPath);
+            if (!output.exists()) output.mkdirs();
 
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
             contentStream.beginText();
--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -1,22 +1,27 @@
 package org.openjfx;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.converter.Converter;
+
 import java.io.File;
 
 public class App extends Application {
 
     private TextField inputFileField;
     private TextField outputFileField;
-
+    private ComboBox<String> inputFormatComboBox;
+    private ComboBox<String> outputFormatComboBox;
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("File Converter");
@@ -36,21 +41,33 @@ public class App extends Application {
         inputBrowseButton.setOnAction(this::browseInputFile);
         grid.add(inputBrowseButton, 2, 0);
 
+        Label inputFormatLabel = new Label("Input Format:");
+        grid.add(inputFormatLabel, 0, 1);
+
+        inputFormatComboBox = new ComboBox<>(FXCollections.observableArrayList("Text", "CSV", "PDF", "PNG", "Extract text from PNG. English", "Extract text from PNG. Estonian")); // Add more options as needed
+        grid.add(inputFormatComboBox, 1, 1);
+
         Label outputFileLabel = new Label("Output File:");
-        grid.add(outputFileLabel, 0, 1);
+        grid.add(outputFileLabel, 0, 2);
 
         outputFileField = new TextField();
-        grid.add(outputFileField, 1, 1);
+        grid.add(outputFileField, 1, 2);
 
         Button outputBrowseButton = new Button("Browse");
         outputBrowseButton.setOnAction(this::browseOutputFile);
-        grid.add(outputBrowseButton, 2, 1);
+        grid.add(outputBrowseButton, 2, 2);
+
+        Label outputFormatLabel = new Label("Output Format:");
+        grid.add(outputFormatLabel, 0, 3);
+
+        outputFormatComboBox = new ComboBox<>(FXCollections.observableArrayList("Text", "CSV", "PDF", "PNG")); // Add more options as needed
+        grid.add(outputFormatComboBox, 1, 3);
 
         Button convertButton = new Button("Convert");
         convertButton.setOnAction(this::convertFile);
-        grid.add(convertButton, 1, 2);
+        grid.add(convertButton, 1, 4);
 
-        Scene scene = new Scene(grid, 400, 150);
+        Scene scene = new Scene(grid, 400, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -72,7 +89,68 @@ public class App extends Application {
     }
 
     private void convertFile(ActionEvent event) {
-        // converter
+        String inputFilePath = inputFileField.getText();
+        String outputFilePath = outputFileField.getText();
+        String inputFormat = inputFormatComboBox.getValue();
+        String outputFormat = outputFormatComboBox.getValue();
+
+        switch (inputFormat) {
+            case "CSV":
+                switch (outputFormat) {
+                    case "PDF":
+                        Converter.SCVtoPDF(inputFilePath, outputFilePath);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "PDF":
+                switch (outputFormat) {
+                    case "PNG":
+                        Converter.PDFToPNG(inputFilePath, outputFilePath);
+                        break;
+                    case "Text":
+                        // add pdf to text
+                        break;
+                    case "CSV":
+                        Converter.extractPDFspreadsheets(inputFilePath, outputFilePath);
+                        break;
+                    default:
+                        //
+                        break;
+                }
+                break;
+            case "PNG":
+                switch (outputFormat) {
+                    case "PDF":
+                        // png to pdf
+                        break;
+
+                    default:
+                        //
+                        break;
+                }
+                break;
+            case "Extract text from PNG. English":
+                switch (outputFormat) {
+                    case "PDF":
+                        Converter.PNGTextToPDFConverter(inputFilePath, outputFilePath, "eng");
+                        break;
+                    default:
+                        break;
+                }
+            case "Extract text from PNG. Estonian":
+                switch (outputFormat) {
+                    case "PDF":
+                        Converter.PNGTextToPDFConverter(inputFilePath, outputFilePath, "est");
+                        break;
+                    default:
+                        break;
+                }
+            default:
+
+                break;
+        }
     }
 
     public static void main(String[] args) {
